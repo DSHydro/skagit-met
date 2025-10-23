d1de84916a24f7e5f9a56c9051751a8ab03620f3
Scott Henderson <3924836+scottyhq@users.noreply.github.com>
2025-10-23 08:54:30 -0700

    apply ruff formatting to scripts

--- scripts/hrrr_downloader.py
+++ scripts/hrrr_downloader.py
@@ -9,124 +9,152 @@ import cfgrib
 import argparse
 import os
 
+
 # Parse command arguments from script run in the command line
 def setupArgs() -> None:
-    parser = argparse.ArgumentParser(description='Download HRRR data using Herbie, and segment to a specific geographic region')
-    parser.add_argument('--model', 
-                        default='hrrr',
-                        type=str,
-                        required=False,
-                        help='Model of data to download, defaults to hrrr')
-    parser.add_argument('--product', 
-                        default='sfc',
-                        type=str,
-                        required=False,
-                        help='Model product to download, defuaults to surface level features')
-    parser.add_argument('--parameters', 
-                        type=str,
-                        required=True,
-                        help='Comma seperated tring containing the variables and level of the vars that will be downloaded e.g. TMP:surface,RH:2 m above ground')
-    parser.add_argument('--startDate', 
-                        default='2020-01-01',
-                        type=str,
-                        required=True,
-                        help='Start date of data to download e.g. 2020-10-01 for October 1, 2020')
-    parser.add_argument('--endDate', 
-                        default='2020-01-02',
-                        type=str,
-                        required=True,
-                        help='End date of data to download e.g. 2020-10-01 for October 1, 2020')
-    parser.add_argument('--geoJson', 
-                        default='data/GIS/SkagitBoundary.json',
-                        type=str,
-                        required=False,
-                        help='Path to/name of geo_json file that geogrpahically limits the downloaded data')
-    parser.add_argument('--outputDir', 
-                        default='data/weather_data/',
-                        type=str,
-                        help='Directory/path to download data/output zarr to.')
-    return parser.parse_args()
+  parser = argparse.ArgumentParser(
+    description="Download HRRR data using Herbie, and segment to a specific geographic region"
+  )
+  parser.add_argument(
+    "--model",
+    default="hrrr",
+    type=str,
+    required=False,
+    help="Model of data to download, defaults to hrrr",
+  )
+  parser.add_argument(
+    "--product",
+    default="sfc",
+    type=str,
+    required=False,
+    help="Model product to download, defuaults to surface level features",
+  )
+  parser.add_argument(
+    "--parameters",
+    type=str,
+    required=True,
+    help="Comma seperated tring containing the variables and level of the vars that will be downloaded e.g. TMP:surface,RH:2 m above ground",
+  )
+  parser.add_argument(
+    "--startDate",
+    default="2020-01-01",
+    type=str,
+    required=True,
+    help="Start date of data to download e.g. 2020-10-01 for October 1, 2020",
+  )
+  parser.add_argument(
+    "--endDate",
+    default="2020-01-02",
+    type=str,
+    required=True,
+    help="End date of data to download e.g. 2020-10-01 for October 1, 2020",
+  )
+  parser.add_argument(
+    "--geoJson",
+    default="data/GIS/SkagitBoundary.json",
+    type=str,
+    required=False,
+    help="Path to/name of geo_json file that geogrpahically limits the downloaded data",
+  )
+  parser.add_argument(
+    "--outputDir",
+    default="data/weather_data/",
+    type=str,
+    help="Directory/path to download data/output zarr to.",
+  )
+  return parser.parse_args()
 
-def getFastHerbie(start_date: str, end_date: str, model: str, product: str, save_dir: str ) -> FastHerbie:
-    date_range = pd.date_range(
-        start=start_date,
-        end=end_date,
-        freq="1h"
-    )
-    return FastHerbie(date_range, model=model, product=product, fxx=range(0,2), save_dir=save_dir)
+
+def getFastHerbie(
+  start_date: str, end_date: str, model: str, product: str, save_dir: str
+) -> FastHerbie:
+  date_range = pd.date_range(start=start_date, end=end_date, freq="1h")
+  return FastHerbie(date_range, model=model, product=product, fxx=range(0, 2), save_dir=save_dir)
+
 
 # Parse GeoJson File into tuple containing boundaries
 def parseGeoJson(geojson_path: str) -> tuple[float, float, float, float]:
-    mask = gpd.read_file(geojson_path)
-    minLon, minLat, maxLon, maxLat = mask.total_bounds
-    return (minLon, maxLon, minLat, maxLat)
+  mask = gpd.read_file(geojson_path)
+  minLon, minLat, maxLon, maxLat = mask.total_bounds
+  return (minLon, maxLon, minLat, maxLat)
+
 
 def limitGeographicRange(bounds: tuple[float, float, float, float], subsetFiles: list) -> list:
-    return [wgrib2.region(f, bounds, name='skagit-basin') for f in subsetFiles]
+  return [wgrib2.region(f, bounds, name="skagit-basin") for f in subsetFiles]
+
 
 # Use Fast herbie to subset and download parameters
 def downloadParameters(parameters: list[str], fh: FastHerbie) -> list[Herbie]:
-    fields = [f":{param}" for param in  parameters]
-    param_regex = fr"^(?:{'|'.join(fields)})"
-    print("Search String: " + param_regex)
-    return fh.download(param_regex)
+  fields = [f":{param}" for param in parameters]
+  param_regex = rf"^(?:{'|'.join(fields)})"
+  print("Search String: " + param_regex)
+  return fh.download(param_regex)
+
 
 def parseParameters(paramString: str) -> list[str]:
-    return paramString.split(',')
+  return paramString.split(",")
 
-def cleanUpFiles(subsetFiles:list) -> None:
-    [os.unlink(f) for f in subsetFiles]
+
+def cleanUpFiles(subsetFiles: list) -> None:
+  [os.unlink(f) for f in subsetFiles]
+
 
 def maskDataset(ds: xr.Dataset, mask_file: str) -> xr.Dataset:
-    mask_shape = gpd.read_file(mask_file)
-    mask = shapely.contains_xy(mask_shape.geometry[0], ds.longitude.values, ds.latitude.values)
-    masked_data_set = ds.where(mask)
+  mask_shape = gpd.read_file(mask_file)
+  mask = shapely.contains_xy(mask_shape.geometry[0], ds.longitude.values, ds.latitude.values)
+  masked_data_set = ds.where(mask)
 
-    return masked_data_set
+  return masked_data_set
+
 
 def mergeDatasets(regionSubsetGribFiles: list) -> xr.Dataset:
-    datasets = []
-    dropVars = ["surface", "heightAboveGround", "valid_time", "step"]
-    # if f001, grab just the accumlated precip by dropping the other forecast variables
-    dropVarsStep = dropVars + ["t", "r2", "si10", "sdswrf", "sdlwrf"]
-    for f in regionSubsetGribFiles:
-        unMergedDatasets = cfgrib.open_datasets(f, indexpath='', decode_timedelta=False)
-        mergedDataset = xr.merge([ds.drop_vars(dropVarsStep, errors="ignore") if ds.step.values == np.timedelta64(1, 'h') else ds.drop_vars(dropVars, errors="ignore") for ds in unMergedDatasets])
-        mergedDataset.load()
-        datasets.append(mergedDataset)
+  datasets = []
+  dropVars = ["surface", "heightAboveGround", "valid_time", "step"]
+  # if f001, grab just the accumlated precip by dropping the other forecast variables
+  dropVarsStep = dropVars + ["t", "r2", "si10", "sdswrf", "sdlwrf"]
+  for f in regionSubsetGribFiles:
+    unMergedDatasets = cfgrib.open_datasets(f, indexpath="", decode_timedelta=False)
+    mergedDataset = xr.merge(
+      [
+        ds.drop_vars(dropVarsStep, errors="ignore")
+        if ds.step.values == np.timedelta64(1, "h")
+        else ds.drop_vars(dropVars, errors="ignore")
+        for ds in unMergedDatasets
+      ]
+    )
+    mergedDataset.load()
+    datasets.append(mergedDataset)
 
-    other_vars = [ds for ds in datasets if 'tp' not in ds.variables]
-    tp_f001 = [ds for ds in datasets if 'tp' in ds.variables]
+  other_vars = [ds for ds in datasets if "tp" not in ds.variables]
+  tp_f001 = [ds for ds in datasets if "tp" in ds.variables]
 
-    tp_ds = xr.concat(tp_f001, dim='time')
-    other_ds = xr.concat(other_vars, dim='time')
-    combined_ds = xr.combine_by_coords([tp_ds, other_ds], compat='override')
-    # Set Longitude to be in correct space
-    combined_ds['longitude'] = combined_ds.longitude-360
+  tp_ds = xr.concat(tp_f001, dim="time")
+  other_ds = xr.concat(other_vars, dim="time")
+  combined_ds = xr.combine_by_coords([tp_ds, other_ds], compat="override")
+  # Set Longitude to be in correct space
+  combined_ds["longitude"] = combined_ds.longitude - 360
 
-    return combined_ds
+  return combined_ds
 
-def write_to_zarr(dataset:xr.Dataset, output_dir: str, path:str) -> None:
-    if output_dir[-1] == '/':
-        output_dir = output_dir[:-1]
 
-    dataset.to_zarr(output_dir + '/' + path, mode='w')
+def write_to_zarr(dataset: xr.Dataset, output_dir: str, path: str) -> None:
+  if output_dir[-1] == "/":
+    output_dir = output_dir[:-1]
+
+  dataset.to_zarr(output_dir + "/" + path, mode="w")
+
 
 if __name__ == "__main__":
-    # Get Arguments - model, variables, product, date range, and geo_json
-    args = setupArgs()
-    parameters = parseParameters(args.parameters)
-    fh = getFastHerbie(args.startDate, args.endDate, args.model, args.product, args.outputDir)
-    fh_files = downloadParameters(parameters, fh)
-    bounds = parseGeoJson(args.geoJson)
-    geo_limited_files = limitGeographicRange(bounds, fh_files)
-    mergedDs = mergeDatasets(geo_limited_files)
-    maskedDs = maskDataset(mergedDs, args.geoJson)
-    write_to_zarr(maskedDs, args.outputDir, args.startDate + '_' + args.endDate + '_HRRR_data.zarr')
-    cleanUpFiles(fh_files)
-    cleanUpFiles([str(f) + '.idx' for f in geo_limited_files])
-    cleanUpFiles(geo_limited_files)
-
-
-
-    
+  # Get Arguments - model, variables, product, date range, and geo_json
+  args = setupArgs()
+  parameters = parseParameters(args.parameters)
+  fh = getFastHerbie(args.startDate, args.endDate, args.model, args.product, args.outputDir)
+  fh_files = downloadParameters(parameters, fh)
+  bounds = parseGeoJson(args.geoJson)
+  geo_limited_files = limitGeographicRange(bounds, fh_files)
+  mergedDs = mergeDatasets(geo_limited_files)
+  maskedDs = maskDataset(mergedDs, args.geoJson)
+  write_to_zarr(maskedDs, args.outputDir, args.startDate + "_" + args.endDate + "_HRRR_data.zarr")
+  cleanUpFiles(fh_files)
+  cleanUpFiles([str(f) + ".idx" for f in geo_limited_files])
+  cleanUpFiles(geo_limited_files)
--- scripts/ornl_downloader.py
+++ scripts/ornl_downloader.py
@@ -11,145 +11,198 @@ import glob
 import argparse
 import helper.ornl_mapper as mapper
 
+
 # Parse command arguments from script run in the command line
 def setupArgs() -> None:
-    parser = argparse.ArgumentParser(description='Download Daily ORNL 4KM downsampled data and clip to region and save as zarr. See https://hydrosource.ornl.gov/data/datasets/9505v3_1/')
-    parser.add_argument('--parameters', 
-                        type=str,
-                        default='',
-                        help='Comma seperated string containing the variables that will be downloaded - defaults to prcp,tmax,tmin,wind,rhum,srad,lrad')
-    parser.add_argument('--startYear', 
-                        type=str,
-                        required=True,
-                        help='Start year of daily data to download e.g. 1999')
-    parser.add_argument('--endYear', 
-                        type=str,
-                        required=True,
-                        help='End year of data to download e.g 2001')
-    parser.add_argument('--outputDir', 
-                        default=mapper.DEFAULT_OUTPUT_PATH,
-                        type=str,
-                        help='Directory/path to download data/output zarr to.')
-    parser.add_argument('--geojson', 
-                        default=mapper.DEFAULT_SKAGIT_GEOJSON,
-                        type=str,
-                        help='Path to/name of geo_json file that geogrpahically limits the downloaded data')
-    parser.add_argument('--reference', 
-                        default=mapper.DEFAULT_REF_SIM,
-                        type=str,
-                        choices=mapper.ALLOWED_REF_MET_OBS,
-                        help="""Reference meteorological observations to use e.g. DaymetV4 or Livneh
+  parser = argparse.ArgumentParser(
+    description="Download Daily ORNL 4KM downsampled data and clip to region and save as zarr. See https://hydrosource.ornl.gov/data/datasets/9505v3_1/"
+  )
+  parser.add_argument(
+    "--parameters",
+    type=str,
+    default="",
+    help="Comma seperated string containing the variables that will be downloaded - defaults to prcp,tmax,tmin,wind,rhum,srad,lrad",
+  )
+  parser.add_argument(
+    "--startYear", type=str, required=True, help="Start year of daily data to download e.g. 1999"
+  )
+  parser.add_argument(
+    "--endYear", type=str, required=True, help="End year of data to download e.g 2001"
+  )
+  parser.add_argument(
+    "--outputDir",
+    default=mapper.DEFAULT_OUTPUT_PATH,
+    type=str,
+    help="Directory/path to download data/output zarr to.",
+  )
+  parser.add_argument(
+    "--geojson",
+    default=mapper.DEFAULT_SKAGIT_GEOJSON,
+    type=str,
+    help="Path to/name of geo_json file that geogrpahically limits the downloaded data",
+  )
+  parser.add_argument(
+    "--reference",
+    default=mapper.DEFAULT_REF_SIM,
+    type=str,
+    choices=mapper.ALLOWED_REF_MET_OBS,
+    help="""Reference meteorological observations to use e.g. DaymetV4 or Livneh
                         Default is DaymetV4, if only ths is priovided, will use simulation driven only from the reference data
-                        If using a GCM, include the GCM name, climate scenario, and downscaling method""")
-    parser.add_argument('--hydroModel', 
-                        default=mapper.DEFAULT_HYDRO,
-                        choices=mapper.ALLOWED_HYDRO_MODELS,
-                        type=str,
-                        help='Hydro model to use e.g. VIC4 (currently only one supported)')
-    parser.add_argument('--gcm', 
-                        choices=mapper.ALLOWED_GCMS,
-                        type=str,
-                        help='Global climate model to use e.g. ACCESS-CM2, CNRM-ESM-1, etc')
-    parser.add_argument('--climateScenario', 
-                        choices=mapper.ALLOWED_CLIMATE_SCENARIOS,
-                        type=str,
-                        help='Climate scenario to use e.g. ssp585, ssp245, etc')
-    parser.add_argument('--downscalingMethod', 
-                        choices=mapper.ALLOWED_DOWNSCALING_METHODS,
-                        type=str,
-                        help='Downscaling method used to downscale GCM data to 4KM resolution, e.g. DBCCA')
-    return parser.parse_args()
-    
+                        If using a GCM, include the GCM name, climate scenario, and downscaling method""",
+  )
+  parser.add_argument(
+    "--hydroModel",
+    default=mapper.DEFAULT_HYDRO,
+    choices=mapper.ALLOWED_HYDRO_MODELS,
+    type=str,
+    help="Hydro model to use e.g. VIC4 (currently only one supported)",
+  )
+  parser.add_argument(
+    "--gcm",
+    choices=mapper.ALLOWED_GCMS,
+    type=str,
+    help="Global climate model to use e.g. ACCESS-CM2, CNRM-ESM-1, etc",
+  )
+  parser.add_argument(
+    "--climateScenario",
+    choices=mapper.ALLOWED_CLIMATE_SCENARIOS,
+    type=str,
+    help="Climate scenario to use e.g. ssp585, ssp245, etc",
+  )
+  parser.add_argument(
+    "--downscalingMethod",
+    choices=mapper.ALLOWED_DOWNSCALING_METHODS,
+    type=str,
+    help="Downscaling method used to downscale GCM data to 4KM resolution, e.g. DBCCA",
+  )
+  return parser.parse_args()
+
+
 def pull_from_globus(url: str) -> str:
-    local_path = fsspec.open_local(f"simplecache::{url}", simplecache={'cache_storage': '/tmp/fsspec_cache'}, same_names=True)
-    return local_path
+  local_path = fsspec.open_local(
+    f"simplecache::{url}", simplecache={"cache_storage": "/tmp/fsspec_cache"}, same_names=True
+  )
+  return local_path
 
-def create_ornl_dataset(start_year: str, end_year: str, dest_path: str, geojson: str, reference: str, gcm: str, climate_scenario: str, downscaling_method: str) -> xr.Dataset:
-    
-    ref = False
-    if gcm is None or climate_scenario is None or downscaling_method is None:
-        ref = True
-    
-    #Output Zarr
-    output_file = f'{dest_path}/{start_year}_{end_year}{"_ref_" + reference  if ref else ""}{"_"+ gcm + "_" + climate_scenario + "_" + downscaling_method if not ref else ""}_ORNL_data.zarr'
-    
-    # Collect Individual Variable Data arrays
-    rasters = []
-    mask = gpd.read_file(geojson)
-    nc_files = glob.iglob(os.path.join('/tmp/fsspec_cache/', '*.nc'))
-    weather_dataset = None
 
-    for f in nc_files:
-        # open weather file and clip to watershed boundaries
-        try:
-            raster = rxr.open_rasterio(f, masked=True)
-            raster = raster.rio.write_crs(mask.crs)
-            raster = raster.rio.clip(mask.geometry)
-        except Exception as e:
-            print(f'Error opening {f}: {e}\n Trying to continue...')
-            continue
+def create_ornl_dataset(
+  start_year: str,
+  end_year: str,
+  dest_path: str,
+  geojson: str,
+  reference: str,
+  gcm: str,
+  climate_scenario: str,
+  downscaling_method: str,
+) -> xr.Dataset:
+  ref = False
+  if gcm is None or climate_scenario is None or downscaling_method is None:
+    ref = True
 
-        #add timestamp to list
-        rasters.append(raster)
-    
-    weather_dataset = xr.merge(rasters)
-    weather_dataset = weather_dataset.rename({'x': 'lon', 'y': 'lat'})
-    weather_dataset = weather_dataset.drop_vars('spatial_ref')
-    weather_dataset['time'] = weather_dataset.time.dt.floor('D')
-    weather_dataset.to_zarr(output_file, mode='w')
-    
-    return weather_dataset
+  # Output Zarr
+  output_file = f"{dest_path}/{start_year}_{end_year}{'_ref_' + reference if ref else ''}{'_' + gcm + '_' + climate_scenario + '_' + downscaling_method if not ref else ''}_ORNL_data.zarr"
+
+  # Collect Individual Variable Data arrays
+  rasters = []
+  mask = gpd.read_file(geojson)
+  nc_files = glob.iglob(os.path.join("/tmp/fsspec_cache/", "*.nc"))
+  weather_dataset = None
+
+  for f in nc_files:
+    # open weather file and clip to watershed boundaries
+    try:
+      raster = rxr.open_rasterio(f, masked=True)
+      raster = raster.rio.write_crs(mask.crs)
+      raster = raster.rio.clip(mask.geometry)
+    except Exception as e:
+      print(f"Error opening {f}: {e}\n Trying to continue...")
+      continue
+
+    # add timestamp to list
+    rasters.append(raster)
+
+  weather_dataset = xr.merge(rasters)
+  weather_dataset = weather_dataset.rename({"x": "lon", "y": "lat"})
+  weather_dataset = weather_dataset.drop_vars("spatial_ref")
+  weather_dataset["time"] = weather_dataset.time.dt.floor("D")
+  weather_dataset.to_zarr(output_file, mode="w")
+
+  return weather_dataset
+
 
 def parseParameters(paramString: str) -> list[str]:
-    param_list = paramString.split(',')
-    if not param_list[0]:
-        return mapper.DEFAULT_VARIABLES
-    
-    for var in param_list:
-        if var not in mapper.ALLOWED_VARIABLES:
-            print(f'Variable {var} not found in allowed variables list. Please check the variable name.')
-            print(f'Removing {var} from the list of variables and continuing with download.')
-            param_list.remove(var)
-    
-    return param_list
+  param_list = paramString.split(",")
+  if not param_list[0]:
+    return mapper.DEFAULT_VARIABLES
 
-def clean_up_files(files:list) -> None:
-    [pathlib.Path(f).unlink(missing_ok=True) for f in files]
+  for var in param_list:
+    if var not in mapper.ALLOWED_VARIABLES:
+      print(f"Variable {var} not found in allowed variables list. Please check the variable name.")
+      print(f"Removing {var} from the list of variables and continuing with download.")
+      param_list.remove(var)
+
+  return param_list
+
+
+def clean_up_files(files: list) -> None:
+  [pathlib.Path(f).unlink(missing_ok=True) for f in files]
+
 
 if __name__ == "__main__":
-    # Get Arguments - model, variables, product, date range, and geo_json
-    args = setupArgs()
-    parameters = parseParameters(args.parameters)
-    output_dir = args.outputDir
-    if output_dir[-1] == '/':
-        output_dir = output_dir[:-1]
+  # Get Arguments - model, variables, product, date range, and geo_json
+  args = setupArgs()
+  parameters = parseParameters(args.parameters)
+  output_dir = args.outputDir
+  if output_dir[-1] == "/":
+    output_dir = output_dir[:-1]
 
-    files = mapper.generate_file_names(args.reference, args.hydroModel, parameters, args.startYear, args.endYear, args.gcm, args.climateScenario, args.downscalingMethod)
-    start_time = dt.now()
-    with ThreadPoolExecutor(max_workers=4) as executor:
-        futures = [executor.submit(pull_from_globus, file) for file in files]
-        wait(futures)
+  files = mapper.generate_file_names(
+    args.reference,
+    args.hydroModel,
+    parameters,
+    args.startYear,
+    args.endYear,
+    args.gcm,
+    args.climateScenario,
+    args.downscalingMethod,
+  )
+  start_time = dt.now()
+  with ThreadPoolExecutor(max_workers=4) as executor:
+    futures = [executor.submit(pull_from_globus, file) for file in files]
+    wait(futures)
 
-    def downloaded_files(futures: list) -> list:
-        downloaded_files = []
-        for f in futures:
-            try:
-                downloaded_files.append(f.result())
-            except Exception as e:
-                print(f"Error downloading file: {f}, exception: {e}")
-                pass
+  def downloaded_files(futures: list) -> list:
+    downloaded_files = []
+    for f in futures:
+      try:
+        downloaded_files.append(f.result())
+      except Exception as e:
+        print(f"Error downloading file: {f}, exception: {e}")
+        pass
 
-        return downloaded_files
+    return downloaded_files
 
-    downloaded_files = downloaded_files(futures)
-    end_time = dt.now()
+  downloaded_files = downloaded_files(futures)
+  end_time = dt.now()
 
-    print('Time to download {} files: {} seconds'.format(len(downloaded_files), (end_time - start_time).seconds))
+  print(
+    "Time to download {} files: {} seconds".format(
+      len(downloaded_files), (end_time - start_time).seconds
+    )
+  )
 
-    # Create Dataset, write out to zarr
-    create_ornl_dataset(args.startYear, args.endYear, output_dir, args.geojson,\
-                        args.reference, args.gcm, args.climateScenario, args.downscalingMethod)
+  # Create Dataset, write out to zarr
+  create_ornl_dataset(
+    args.startYear,
+    args.endYear,
+    output_dir,
+    args.geojson,
+    args.reference,
+    args.gcm,
+    args.climateScenario,
+    args.downscalingMethod,
+  )
 
-    # cleanup
-    to_clean = glob.iglob(os.path.join('/tmp/fsspec_cache/', '*.nc'))
-    clean_up_files(to_clean)
+  # cleanup
+  to_clean = glob.iglob(os.path.join("/tmp/fsspec_cache/", "*.nc"))
+  clean_up_files(to_clean)
